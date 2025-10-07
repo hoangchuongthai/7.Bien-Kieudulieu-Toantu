624389572092d8d52c37fa71bfc031e96b806e98
chuongite <simane161@gmail.com>
2025-10-07 22:32:46 +0700

    Submit bai tap 3 [Thuc hanh]Khai bao va su dung bien

--- su-dung-bien_exercise3.js
+++ su-dung-bien_exercise3.js
@@ -1,4 +1,10 @@
-let width = 20
-let height = 10
-let area = width * height
-document.write('Area = ' + area)
+let a = prompt("Enter a:");
+let b = prompt("Enter b:");
+
+if (b === 0) {
+    alert("Division by zero is not allowed.");
+} else if (a % b === 0) {
+    alert(`${a} is a multiple of ${b}.`);
+} else {
+    alert(`${a} is not a multiple of ${b}.`);
+}
